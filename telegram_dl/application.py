b12b5f853095d8ab2fe533a624b68c681a20fe3f
Mark Grandi <markgrandi@gmail.com>
2019-12-28 22:44:32 -0700

    can now round trip a tdlib_generated object to and from JSON
    we implement (really hackily i will add) a custom cattr.Converter
    that adds the @type key/value when converting to JSON and uses it
    when converting from JSON
    
    the cattr library is really not the best library for this, i should
    just write my own thing and save a lot of boilerplate code, and be
    able to get rid of the useless 'class' argument when converting from
    JSON, since we use the @type to figure out the type of the object

--- telegram_dl/application.py
+++ telegram_dl/application.py
@@ -1,10 +1,11 @@
 import logging
 
 
-import telegram_dl.tdlib as tdlib
+from telegram_dl import tdlib
+from telegram_dl import utils
+from telegram_dl import tdlib_generated
 
 logger = logging.getLogger(__name__)
-
 
 
 class Application:
@@ -19,72 +20,106 @@ class Application:
 
         self.args = args
 
-        tdlib_config = tdlib.TdlibConfiguration.init_from_config(self.args.config)
-
-        self.tdlib_handle = tdlib.TdlibHandle.init_from_config(tdlib_config)
-
-    def run(self):
 
 
-        # start the tdlib session
 
-        self.tdlib_handle = self.tdlib_handle.create_client()
+    async def run(self):
+
+        from telegram_dl import tdlib_generated as tdg
+
+        log_stream_file_obj = tdg.logStreamFile(
+            path="C:/Users/auror/Temp/log.txt",
+            max_file_size=10000000)
+        set_log_stream_obj = tdg.setLogStream(log_stream=log_stream_file_obj)
+
+
+        logger.info("original: `%s`", set_log_stream_obj)
+        converter = utils.CustomCattrConverter(tdlib_generated.tdlib_gen_globals, tdlib_generated.tdlib_gen_locals)
+
+        # utils.register_tdlib_gen_classes_with_cattr(converter)
+
+        x =converter.unstructure(set_log_stream_obj)
+
+        logger.info("unstructure result: `%s`", x)
 
         import pdb;pdb.set_trace()
+        y = converter.structure(x, tdg.RootObject)
 
-        while True:
-            event = self.tdlib_handle.receive()
+        logger.info("structure result: `%s`", y)
 
-            logger.debug("new event: `%s`", event)
+        # # start the tdlib session
+        # tdlib_config = tdlib.TdlibConfiguration.init_from_config(self.args.config)
+        # self.tdlib_handle = await tdlib.TdlibHandle.init_from_config(tdlib_config)
 
-            if event:
+        # self.tdlib_handle = await self.tdlib_handle.create_client()
 
-                 # process authorization states
-                if event['@type'] == 'updateAuthorizationState':
-                    auth_state = event['authorization_state']
 
-                    # if client is closed, we need to destroy it and create new client
-                    if auth_state['@type'] == 'authorizationStateClosed':
-                        break
+        # while True:
+        #     logger.debug("loop iteration")
+        #     resultdict = await self.tdlib_handle.receive()
 
-                    # set TDLib parameters
-                    # you MUST obtain your own api_id and api_hash at https://my.telegram.org
-                    # and use them in the setTdlibParameters call
-                    if auth_state['@type'] == 'authorizationStateWaitTdlibParameters':
-
-                        tdlibParams = {'@type': 'setTdlibParameters',
-                            'parameters': self.tdlib_handle.tdlib_config.get_tdlibparams_json_params()}
-                        self.tdlib_handle.send(tdlibParams)
-
-                    # set an encryption key for database to let know TDLib how to open the database
-                    if auth_state['@type'] == 'authorizationStateWaitEncryptionKey':
-                        self.tdlib_handle.send({'@type': 'checkDatabaseEncryptionKey', 'key': ''})
-
-                    # enter phone number to log in
-                    if auth_state['@type'] == 'authorizationStateWaitPhoneNumber':
-                        phone_number = input('Please enter your phone number: ')
-                        self.tdlib_handle.send({'@type': 'setAuthenticationPhoneNumber', 'phone_number': phone_number})
-
-                    # wait for authorization code
-                    if auth_state['@type'] == 'authorizationStateWaitCode':
-                        code = input('Please enter the authentication code you received: ')
-                        self.tdlib_handle.send({'@type': 'checkAuthenticationCode', 'code': code})
-
-                    # wait for first and last name for new users
-                    if auth_state['@type'] == 'authorizationStateWaitRegistration':
-                        first_name = input('Please enter your first name: ')
-                        last_name = input('Please enter your last name: ')
-                        self.tdlib_handle.send({'@type': 'registerUser', 'first_name': first_name, 'last_name': last_name})
-
-                    # wait for password if present
-                    if auth_state['@type'] == 'authorizationStateWaitPassword':
-                        password = input('Please enter your password: ')
-                        self.tdlib_handle.send({'@type': 'checkAuthenticationPassword', 'password': password})
+        #     logger.debug("recieved something from receive: `%s`", resultdict)
 
 
 
 
-        self.tdlib_handle = self.tdlib_handle.destroy_client()
+
+
+        # import pdb;pdb.set_trace()
+
+        # while True:
+        #     event = self.tdlib_handle.receive()
+
+        #     logger.debug("new event: `%s`", event)
+
+        #     if event:
+
+        #          # process authorization states
+        #         if event['@type'] == 'updateAuthorizationState':
+        #             auth_state = event['authorization_state']
+
+        #             # if client is closed, we need to destroy it and create new client
+        #             if auth_state['@type'] == 'authorizationStateClosed':
+        #                 break
+
+        #             # set TDLib parameters
+        #             # you MUST obtain your own api_id and api_hash at https://my.telegram.org
+        #             # and use them in the setTdlibParameters call
+        #             if auth_state['@type'] == 'authorizationStateWaitTdlibParameters':
+
+        #                 tdlibParams = {'@type': 'setTdlibParameters',
+        #                     'parameters': self.tdlib_handle.tdlib_config.get_tdlibparams_json_params()}
+        #                 self.tdlib_handle.send(tdlibParams)
+
+        #             # set an encryption key for database to let know TDLib how to open the database
+        #             if auth_state['@type'] == 'authorizationStateWaitEncryptionKey':
+        #                 self.tdlib_handle.send({'@type': 'checkDatabaseEncryptionKey', 'key': ''})
+
+        #             # enter phone number to log in
+        #             if auth_state['@type'] == 'authorizationStateWaitPhoneNumber':
+        #                 phone_number = input('Please enter your phone number: ')
+        #                 self.tdlib_handle.send({'@type': 'setAuthenticationPhoneNumber', 'phone_number': phone_number})
+
+        #             # wait for authorization code
+        #             if auth_state['@type'] == 'authorizationStateWaitCode':
+        #                 code = input('Please enter the authentication code you received: ')
+        #                 self.tdlib_handle.send({'@type': 'checkAuthenticationCode', 'code': code})
+
+        #             # wait for first and last name for new users
+        #             if auth_state['@type'] == 'authorizationStateWaitRegistration':
+        #                 first_name = input('Please enter your first name: ')
+        #                 last_name = input('Please enter your last name: ')
+        #                 self.tdlib_handle.send({'@type': 'registerUser', 'first_name': first_name, 'last_name': last_name})
+
+        #             # wait for password if present
+        #             if auth_state['@type'] == 'authorizationStateWaitPassword':
+        #                 password = input('Please enter your password: ')
+        #                 self.tdlib_handle.send({'@type': 'checkAuthenticationPassword', 'password': password})
+
+
+
+
+        # self.tdlib_handle = self.tdlib_handle.destroy_client()
 
 
 
--- telegram_dl/utils.py
+++ telegram_dl/utils.py
@@ -1,8 +1,19 @@
 import attr
 import json
 import pathlib
+import logging
+import typing
 
+import cattr
+
+from telegram_dl import tdlib_generated
 from telegram_dl import constants
+
+logger = logging.getLogger(__name__)
+
+converter_logger = logger.getChild("converter")
+structure_logger = converter_logger.getChild("structure")
+unstructure_logger = converter_logger.getChild("unstructure")
 
 class CustomJSONEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -11,6 +22,130 @@ class CustomJSONEncoder(json.JSONEncoder):
 
         # Let the base class default method raise the TypeError
         return json.JSONEncoder.default(self, obj)
+
+class CustomCattrConverter(cattr.Converter):
+    '''
+    subclass of cattr.Converter so we can overwrite a method to make it so that
+    we include the @type entries in the dict
+
+    these were taken from the cattrs source code,` cattrs/src/cattr/converters.py `
+    git revision 0460fbe825a1e7e50919c5d80e412a309dd96c54
+    https://github.com/Tinche/cattrs/blob/0460fbe825a1e7e50919c5d80e412a309dd96c54/src/cattr/converters.py
+
+    '''
+
+    def __init__(self, globals_ref, locals_ref):
+
+        super().__init__()
+
+        self._globals = globals_ref
+        self._locals = locals_ref
+
+    def unstructure_attrs_asdict(self, obj):
+        # type: (Any) -> Dict[str, Any]
+        """Our version of `attrs.asdict`, so we can call back to us."""
+        attrs = obj.__class__.__attrs_attrs__
+        dispatch = self._unstructure_func.dispatch
+        rv = self._dict_factory()
+        for a in attrs:
+            name = a.name
+            v = getattr(obj, name)
+            rv[name] = dispatch(v.__class__)(v)
+
+        # CUSTOM MODIFICATIONS
+        if isinstance(obj, tdlib_generated.RootObject):
+            rv[constants.TDLIB_JSON_TYPE_STR] = getattr(obj, constants.TDLIB_TYPE_VAR_NAME)
+        # END CUSTOM MODIFICATIONS
+
+        return rv
+
+
+    def structure_attrs_fromdict(self, dict_to_unstructure, cl):
+        '''
+        @param dict_to_unstructure - a dictionary, the stuff we are deserializing into an actual object
+        @param cl - the class provided to converter.structure, however since this is our
+            own converter, we are ignoring this and using the types that the JSON has in the
+            `@type` key/value
+        '''
+
+        # type: (Mapping[str, Any], Type[T]) -> T
+        """Instantiate an attrs class from a mapping (dict)."""
+
+
+        # CUSTOM MODIFICATIONS
+
+        structure_logger.debug("structuring, dict_to_unstructure = `%s`, class = `%s`", dict_to_unstructure, cl)
+
+        actual_type = cl
+
+
+        if constants.TDLIB_JSON_TYPE_STR in dict_to_unstructure.keys():
+
+            # TODO should extract this to a separate method
+            stated_type_in_json = dict_to_unstructure[constants.TDLIB_JSON_TYPE_STR]
+            actual_type = eval(stated_type_in_json, self._globals, self._locals)
+
+        structure_logger.debug("using the class `%s` instead of `%s` because `%s` was in the dict we are unstructuring's keys",
+            actual_type, cl, constants.TDLIB_JSON_TYPE_STR)
+
+        # END CUSTOM MODIFICATIONS
+
+        # For public use.
+        conv_obj = {}  # Start with a fresh dict, to ignore extra keys.
+        dispatch = self._structure_func.dispatch
+        structure_logger.debug("iterating over attributes of `%s`", actual_type)
+        for a in actual_type.__attrs_attrs__:  # type: ignore
+            # We detect the type by metadata.
+            name = a.name
+            type_ = a.type
+
+            structure_logger.debug("-- name: `%s`, type: `%s`", name, type_)
+            # CUSTOM MODIFICATIONS
+            if isinstance(type_, str):
+
+                # don't use typing.get_type_hints here it doesn't seem to work for strings
+                # unless you do it in the place that the class was defined *table flip*
+                newtype = eval(type_, self._globals, self._locals)
+
+                structure_logger.debug("---- type is now `%s`, original type was a string, ran eval() on it", newtype)
+                type_ = newtype
+
+            if isinstance(dict_to_unstructure, tdlib_generated.RootObject):
+
+                # we need this because technically every message we get back will be a
+                # subclass of RootObject, and even smaller stuff like LogStream has several subclasses like
+                # `logStreamFile`, but if we use the stated type, we would try and just create the LogStream
+                # object which has nothing (as its essentially an interface) rather than the `logStreamFile`
+                # that 'is a' LogStream
+
+                # TODO should extract this to a different method
+                stated_type_in_json = dict_to_unstructure[constants.TDLIB_JSON_TYPE_STR]
+                newtype = eval(stated_type_in_json, self._globals, self._locals)
+
+                structure_logger.debug("---- new type is now `%s`, original type was a subclass of RootObject so we use the type in the dict's `%s` key/value",
+                    newtype, constants.TDLIB_JSON_TYPE_STR)
+
+                type_ = newtype
+
+            # END CUSTOM MODIFICATIONS
+
+
+            try:
+                val = dict_to_unstructure[name]
+            except KeyError:
+                continue
+
+            if name[0] == "_":
+                name = name[1:]
+
+            structure_logger.debug("---- dispatching to structure type `%s`", type_)
+
+            conv_obj[name] = (
+                dispatch(type_)(val, type_) if type_ is not None else val
+            )
+
+        return actual_type(**conv_obj)  # type: ignore
+
 
 # TAKEN FROM https://github.com/python-attrs/attrs/blob/master/src/attr/_funcs.py
 # REVISION: 8824dc26c219abcb43564dd9386fe1a88f938344
