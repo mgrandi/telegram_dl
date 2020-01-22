997b532f0f63bcbf252fc7d10b7b080bdd2ccd83
Mark Grandi <markgrandi@gmail.com>
2020-01-21 17:56:44 -0700

    various changes to the db_model.py class:
    * renamed the db_model_enums import to be dbme (to be shorter to type)
    * changed all the unique constraints to be unique indexes instead
    cause that is more obvious on what is going on
    * added a few more indexes
    * uncommented and implemented the user.profile_photo_id column,
    added the relationship
    * fixed a bug with sqlalchemy_utils.ChoiceType columns, where
    if you specify `impl=` you NEED to create a instance of the class
    rather than just passing in the class itself, so `impl=Integer()`
    rather than `impl=Integer`
    * apparently `tuple()` can only take 1 argument, a iterable, so
    i just made it use `(x,y,z)` syntax instead

--- telegram_dl/db_model.py
+++ telegram_dl/db_model.py
@@ -5,7 +5,7 @@ from sqlalchemy_repr import RepresentableBase
 
 from sqlalchemy_utils import PhoneNumberType, ChoiceType
 
-import telegram_dl.db_model_enums as tgdl_model_enums
+import telegram_dl.db_model_enums as dbme
 
 CustomDeclarativeBase = declarative_base(cls=RepresentableBase)
 
@@ -26,24 +26,27 @@ class User(CustomDeclarativeBase):
     # i don't really care about the status so i won't store it, or have a UserStatus type really
     # status = Column()
 
-    # profile_photo = Column()
-    outgoing_link = Column(ChoiceType(tgdl_model_enums.LinkStateEnum, impl=Integer))
-    incoming_link = Column(ChoiceType(tgdl_model_enums.LinkStateEnum, impl=Integer))
+    profile_photo_id = Column(Unicode(100), ForeignKey("profile_photo.tg_profile_photo_id"))
+
+    outgoing_link = Column(ChoiceType(dbme.LinkStateEnum, impl=Integer()))
+    incoming_link = Column(ChoiceType(dbme.LinkStateEnum, impl=Integer()))
     is_verified = Column(Boolean)
     is_support = Column(Boolean)
     restriction_reason = Column(Unicode(length=255))
     is_scam = Column(Boolean)
     have_access = Column(Boolean)
 
-    user_type = Column(ChoiceType(tgdl_model_enums.UserTypeEnum, impl=Integer))
+    user_type = Column(ChoiceType(dbme.UserTypeEnum, impl=Integer()))
 
     language_code = Column(Unicode(length=20))
 
-    __table_args__ = tuple(
 
-        # FIXME: does this create an index, or do i need to create a
-        # index as well as a unique constraint?
-        UniqueConstraint("tg_user_id", name="UQ-user-tg_user_id"),
+    profile_photo = relationship("ProfilePhoto")
+
+    __table_args__ = (
+        Index("IXUQ-user-tg_user_id", "tg_user_id", unique=True),
+        Index("IX-user-phone_number", "phone_number"),
+        Index("IX-user-user_type", "user_type"),
     )
 
 
@@ -78,11 +81,10 @@ class File(CustomDeclarativeBase):
     remote_file_id = Column(Unicode(100))
 
 
-    __table_args__ = tuple(
+    __table_args__ = (
 
-        # FIXME: does this create an index, or do i need to create a
-        # index as well as a unique constraint?
-        UniqueConstraint("tg_file_id", name="UQ-file-tg_file_id")
+        Index("IXUQ-file-tg_file_id", "tg_file_id", unique=True),
+        Index("IX-file-remote_file_id", "remote_file_id"),
     )
 
 
@@ -100,11 +102,9 @@ class ProfilePhoto(CustomDeclarativeBase):
     big = relationship("File", foreign_keys=[big_id])
     small = relationship("File", foreign_keys=[small_id])
 
-    __table_args__ = tuple(
+    __table_args__ = (
 
-        # FIXME: does this create an index, or do i need to create a
-        # index as well as a unique constraint?
-        UniqueConstraint("tg_profile_photo_id", name="UQ-profile_photo-tg_profile_photo_id")
+        Index("IXUQ-profile_photo-tg_profile_photo_id", "tg_profile_photo_id"),
     )
 
 
