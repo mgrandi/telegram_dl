6ac57b54f4d5adb09eeba349cf4a04409eb7748b
Mark Grandi <markgrandi@gmail.com>
2020-03-05 21:36:07 -0700

    add methods to parse from tdlib_generated objects to the enum Versions

--- telegram_dl/db_model_enums.py
+++ telegram_dl/db_model_enums.py
@@ -1,4 +1,8 @@
+from __future__ import annotations
+
 import enum
+
+from telegram_dl import tdlib_generated as tdg
 
 # NOTE:
 # i am not sure if it matters too much, but i am going to refrain from using
@@ -10,6 +14,20 @@ class LinkStateEnum(enum.Enum):
     LINK_STATE_NONE = 0
     LINK_STATE_IS_CONTACT = 1
     LINK_STATE_KNOWS_PHONE_NUMBER = 2
+
+
+    @staticmethod
+    def parse_from_tdg_linkstate(tdg_linkstate:tdg.LinkState) -> LinkStateEnum:
+
+        if isinstance(tdg_linkstate, tdg.linkStateNone):
+            return LinkStateEnum.LINK_STATE_NONE
+        elif isinstance(tdg_linkstate, tdg.linkStateKnowsPhoneNumber):
+            return LinkStateEnum.LINK_STATE_KNOWS_PHONE_NUMBER
+        elif isinstance(tdg_linkstate, tdg.linkStateIsContact):
+            return LinkStateEnum.LINK_STATE_IS_CONTACT
+        else:
+            raise Exception("Unknown link state: `%s`", tdg_linkstate)
+
 
 class UserTypeEnum(enum.Enum):
 
@@ -30,3 +48,22 @@ class UserTypeEnum(enum.Enum):
     USER_TYPE_REGULAR = 1
     USER_TYPE_DELETED = 2
     USER_TYPE_BOT = 3
+
+
+    @staticmethod
+    def parse_from_tdg_usertype(tdg_usertype:UserType) -> UserTypeEnum:
+
+        if isinstance(tdg_usertype, tdg.userTypeUnknown):
+            return UserTypeEnum.USER_TYPE_UNKNOWN
+
+        elif isinstance(tdg_usertype, tdg.userTypeRegular):
+            return UserTypeEnum.USER_TYPE_REGULAR
+
+        elif isinstance(tdg_usertype, tdg.userTypeDeleted):
+            return UserTypeEnum.USER_TYPE_DELETED
+
+        elif isinstance(tdg_usertype, tdg.userTypeBot):
+            return UserTypeEnum.USER_TYPE_BOT
+
+        else:
+            raise Exception("Unknown link state: `%s`", tdg_linkstate)
