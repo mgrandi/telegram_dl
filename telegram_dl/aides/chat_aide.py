a1cb10338bef9465a5b044e5251a23f6d488ca4f
Mark Grandi <markgrandi@gmail.com>
2020-05-21 02:02:04 -0700

    add missing equality checks for `db_model.Chat` subclasses in ChatAide
    i was only checking the fields that are in the `Chat` class but didn't
    have equality checks for the extra fields that are in the `Chat`
    subclasses
    
    also fix a typo in checking the number of versions that was
    left over from the migration from db_equality.py

--- telegram_dl/aides/chat_aide.py
+++ telegram_dl/aides/chat_aide.py
@@ -1,5 +1,6 @@
 import logging
 import typing
+import itertools
 
 from telegram_dl import db_model
 from telegram_dl import tdlib_generated as tdg
@@ -35,15 +36,50 @@ class ChatAide:
 
             return fast_result
 
-        if len(dbmodel_chat.tdl_chat.versions) == 0:
+        if len(dbmodel_chat.versions) == 0:
 
             logger.debug("returning False because Chat exists but has no versions")
             return False
 
-        # check the base chat
-        base_chat_comparison = isinstance(dbmodel_chat, db_model.Chat) \
-            and isinstance(tdlib_chat, tdg.chat) \
-            and dbmodel_chat.tg_chat_id == tdlib_chat.id
+        # here we need to check the db_model.Chat subclasses
+        # here is a list of booleans and we will check all of them to make the
+        # final result
+        base_chat_comparison_result_list = list()
+
+        # base chat checks
+        base_chat_comparison_result_list.append(isinstance(dbmodel_chat, db_model.Chat))
+        base_chat_comparison_result_list.append(isinstance(tdlib_chat, tdg.chat))
+        base_chat_comparison_result_list.append(dbmodel_chat.tg_chat_id == tdlib_chat.id)
+
+
+        # chat subclass checks
+        if isinstance(dbmodel_chat, db_model.PrivateChat):
+
+            base_chat_comparison_result_list.append(dbmodel_chat.user_id == tdlib_chat.type.user_id)
+
+        elif isinstance(dbmodel_chat, db_model.SecretChat):
+
+            base_chat_comparison_result_list.append(dbmodel_chat.tg_secret_chat_id == tdlib_chat.type.secret_chat_id)
+            base_chat_comparison_result_list.append(dbmodel_chat.user.tg_user_id == tdlib_chat.type.user_id)
+
+        elif isinstance(dbmodel_chat, db_model.BasicGroupChat):
+
+            base_chat_comparison_result_list.append(dbmodel_chat.tg_basic_group_id == tdlib_chat.type.basic_group_id)
+
+        elif isinstance(dbmodel_chat, db_model.SuperGroupChat):
+
+            base_chat_comparison_result_list.append(dbmodel_chat.tg_super_group_id == tdlib_chat.type.supergroup_id)
+            base_chat_comparison_result_list.append(dbmodel_chat.is_channel == tdlib_chat.type.is_channel)
+
+        else:
+            raise Exception(
+                f"tried to compare a `db_model.Chat` subclass that isn't supported! Chat: `{dbmodel_chat}`")
+
+        # count the number of False booleans in the list, if there are any then the entire predicate
+        # fails. basically a fancy way of iterating and doing a running `x and y` thing
+        # note: If predicate is None, return the items that are false
+        filterfalse_result_list = list(itertools.filterfalse(None, base_chat_comparison_result_list))
+        base_chat_comparison = len(filterfalse_result_list) == 0
 
         # get the latest version
 
